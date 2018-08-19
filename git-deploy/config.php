61a1e097d3563619e175152978f5a58ec69cfe08
drspangle <2737227+drspangle@users.noreply.github.com>
2018-08-19 06:23:33 -0400

    removed git-deploy, no need for a webhook it turns out

--- git-deploy/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define("TOKEN", "52172c4ce13abc58e03d2f9d79e0a941");                                       // The secret token to add as a GitHub or GitLab secret, or otherwise as https://www.example.com/?token=secret-token
-define("REMOTE_REPOSITORY", "git@github.com:drspangle/daniels-page.git"); // The SSH URL to your repository
-define("DIR", "/home/dsmullen/github/daniels-page");                          // The path to your repostiroy; this must begin with a forward slash (/)
-define("BRANCH", "refs/heads/master");                                 // The branch route
-define("LOGFILE", "deploy.log");                                       // The name of the file you want to log to.
-define("GIT", "/usr/bin/git");                                         // The path to the git executable
-define("AFTER_PULL", "");                                              // A command to execute after successfully pulling
